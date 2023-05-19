a923894cc98a032a2ea99b07e5094b9bd567e25a
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-05-19 11:32:54 +0200

    sixth commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,9 @@
 import movies from "../assets/movies.json";
 import Card from "./Card";
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_hor_scroll
+// https://www.w3schools.com/howto/howto_css_menu_horizontal_scroll.asp
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Map
+
 
 const Cards = () => {
     console.log(movies);
